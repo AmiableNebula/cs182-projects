2f136108e1aace6ac34213c0817f283bc7c8698c
Sarah <sarahramseymcneil@gmail.com>
2019-11-14 14:17:40 -0800

    Remove comments

--- Lab18/src/Lab18.java
+++ Lab18/src/Lab18.java
@@ -4,19 +4,15 @@ public class Lab18 {
 	}
 	
 	public static int hashCode (String str) {
-		// Define integer for later use
 		int strInt = 0;
 		try {
-			// Attempt to call parseInt() on the string
 			strInt = Integer.parseInt (str);
 		} catch (NumberFormatException e) {
-			// If parseInt() throws an exception, iterate through each character and add their integer representations
 			for (int i = 0; i < str.length(); i++) {
 				strInt += str.charAt (i) * 2;
 			}
 		}
 		
-		// Return the resulting integer
 		return strInt;
 	}
 }
